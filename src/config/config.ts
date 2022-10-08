8ac242f44c8a4ab555a0a0f3980d31b9a1abfa21
Adam Almqvist <adamalmqvist@MacBook-Air-som-tillhor-Adam.local>
2022-10-08 20:21:04 +0200

    Add functionality for interacting with blockchain

---
+++ src/config/config.ts
@@ -0,0 +1,7 @@
+const dotenv = require("dotenv");
+dotenv.config();
+export const WALLETS = [
+  process.env.FIRST_PRIVATE_KEY,
+  process.env.SECOND_PRIVATE_KEY,
+  process.env.THIRD_PRIVATE_KEY,
+];
